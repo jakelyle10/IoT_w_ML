b39e4d724a297cda8eb99cc8d3faf8078e990e64
jakelyle10 <99229473+jakelyle10@users.noreply.github.com>
2022-09-30 11:21:49 -0500

    Fixing bug

--- Parking_Sensor.ino
+++ Parking_Sensor.ino
@@ -9,7 +9,7 @@
 #include <Grove_ChainableLED.h>     // RGB LED library
 
 float distance1 = 15.0;             // car distance boundary
-float ticker = 0;                 // ticker counter to see how long the car is parked
+int ticker = 0;                 // ticker counter to see how long the car is parked
 
 Ultrasonic ultrasonic(D4);          // Ultrasonic object constructor
 ChainableLED leds(D2, D3, 1);       // LED object constructor
@@ -25,13 +25,13 @@ long RangeInCentimetersPrevious = 15.0;
 void loop()
 {
 	long RangeInCentimeters;        // Variable to hold the distance measurement
-  	long RangeInCentimetersPrevious;        // Variable to hold the distance measurement
 
 
 	RangeInCentimeters = ultrasonic.MeasureInCentimeters();     // Get the current distance value
 	Serial.print(RangeInCentimeters);                           // Out put the value over serial
 	Serial.println(" cm"); // use 'particle serial monitor --follow' in the CLI to see serial output
-	
+
+
 	if (RangeInCentimeters > distance1){                        // If range is greater than far boundary...
 	    leds.setColorRGB(0, 0, 255, 0);                         // green
 	}
@@ -39,13 +39,16 @@ void loop()
 	    leds.setColorRGB(0, 255, 0, 0);                         // red
 	}
 	
-  	if (RangeInCentimeters < RangeInCentimetersPrevious + 2 || RangeInCentimeters > RangeInCentimetersPrevious - 2){
-    ticker = ticker +1;
+  	if (RangeInCentimeters <= RangeInCentimetersPrevious + 3 && RangeInCentimeters >= RangeInCentimetersPrevious - 3){
+    	ticker = ticker +1;
   	}
+	else{
+		ticker = 0;
+	}
 
   	if(ticker == 300){
-	Particle.publish("parking", "parked", PRIVATE);
-	ticker = 0;
+		Particle.publish("parking", "parked", PRIVATE);
+		ticker = 0;
   	}
 
   	RangeInCentimetersPrevious = RangeInCentimeters;
